9ac13138aee7972f2c232a87364681c084fbf193
SofiiaHutsuliak <syoantioa@gmail.com>
2025-04-03 11:27:59 +0300

    Update TextOperations.h

--- Lab1/TextOperations.h
+++ Lab1/TextOperations.h
@@ -2,9 +2,9 @@
 #define TEXTOPERATIONS_H
 #include <string>
 using namespace std;
-namespace TextOperations 
+namespace TextOperations  //namespace declaration
 {
-    string concat(string a, string b);
+    string concat(string a, string b); //function declaration
 }
 
 #endif
