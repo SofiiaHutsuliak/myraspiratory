eda7286fc14e94c6e1a880ccd257f6690729e2ff
SofiiaHutsuliak <syoantioa@gmail.com>
2025-04-03 11:23:01 +0300

    Update Task2main.cpp

--- Lab1/Task2main.cpp
+++ Lab1/Task2main.cpp
@@ -7,18 +7,18 @@ int main()
 {  
     string str1;
     cout << "Enter a string: ";
-    cin >> str1;
-    string str2 = str1;
-    transform(str1.begin(), str1.end(), str1.begin(), ::toupper);
+    cin >> str1; // Storing string entered by user
+    string str2 = str1; // Creating equal string for third funtion
+    transform(str1.begin(), str1.end(), str1.begin(), ::toupper); // Conversion to upper case string
     cout << "Uppercase: " << str1 << endl;
     int vowels = 0;
     for (char c : str1) {
         if (c== 'A' || c=='E' || c=='I' || c=='O' || c== 'U') {
-            vowels++;
+            vowels++;  // for loop to count vowels
         }
     }
     cout << "Number of vowels: " << vowels << endl;
-    reverse(str2.begin(), str2.end());
+    reverse(str2.begin(), str2.end()); // Reversing string
     cout << "Reversed string: " << str2 << endl;
     return 0;
 }
