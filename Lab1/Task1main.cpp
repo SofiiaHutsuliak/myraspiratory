808695e4139299d62acd227585d1236bbc1c4844
SofiiaHutsuliak <syoantioa@gmail.com>
2025-04-03 11:24:16 +0300

    Update Task1main.cpp

--- Lab1/Task1main.cpp
+++ Lab1/Task1main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <string>
-#include "MathOperations.h"
-#include "TextOperations.h"
+#include "MathOperations.h" //header file containing mathoperations namespace
+#include "TextOperations.h" //header file containing textoperations namespace
 using namespace std;
 
 int main() 
 {
-    int num1 = 5, num2 = 10;
-    cout << MathOperations::add(num1, num2)<< endl;
-    string sta = "aa", stb = "bb";
-    cout << TextOperations::concat(sta, stb)<< endl;
+    int num1 = 2, num2 = 11; // initializing 2 numbers
+    cout << MathOperations::add(num1, num2)<< endl;  //calling mathoperations namespace to count sum and printing result
+    string sta = "aa", stb = "bb"; //initializing 2 strings
+    cout << TextOperations::concat(sta, stb)<< endl; //calling textoperations namespace to count sum and printing result
     return 0;
 }
